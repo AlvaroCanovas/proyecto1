1ccda8a1b62841fe4e6febcd60e0586113f20c7e
at <at@PC-5>
2020-11-05 18:13:20 +0100

    Commit Inicial. Paso dos.

---
+++ Prueba1/src/Principal.java
@@ -0,0 +1,12 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hola Mundo");
+		System.out.print("Hola Mundo2");
+		System.out.print("Hola Mundo3");
+
+	}
+
+}
